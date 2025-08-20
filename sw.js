d7985e7f635a0ce0eead3cff47386b8ebc4dd60e
KushagraKarira <kushagrakarira@gmail.com>
2025-08-20 12:38:04 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,56 @@
+// Define a cache name
+const CACHE_NAME = 'jj-fruits-cache-v1';
+
+// List the files to cache
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/Mosambi.jpg',
+  '/Strawberry.jpg',
+  '/Kesar.jpg',
+  '/IMPApples.jpg',
+  'https://jamiatmaljagatram.com/Pineapple.jpg'
+];
+
+// Install the service worker and cache the files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Serve cached content when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        // Not in cache - fetch from network
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Clean up old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
